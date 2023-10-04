dad7d2f27685745bf8bc64952a4d3e6457cfbd7e
Lokesh Mandvekar <lsm5@fedoraproject.org>
2023-10-04 10:25:04 -0400

    Packit: enable podman e2e tests
    The test scripts are replaced by inline scripting in the fmf file itself
    making it much easier to maintain.
    
    Signed-off-by: Lokesh Mandvekar <lsm5@fedoraproject.org>

--- plans/podman_system_test.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Copr repo setup handled in common_setup.sh
-. ./plans/common_setup.sh
-
-# Run Podman's SELinux system tests
-bats /usr/bin/podman /usr/share/podman/test/system/410-selinux.bats
