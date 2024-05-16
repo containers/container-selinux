6bbc2a2e970fd0e8013c09d42f28f9b3785e7e5b
Lokesh Mandvekar <lsm5@redhat.com>
2024-05-16 16:01:57 -0400

    TMT: Minimize distro conditionals
    Differences across distros and versions in components like dnf as well
    as variable package availability for bats makes idiomatic TMT hard to
    maintain if we're to install all dependencies using rpm packages.
    
    Fetching `bats` from upstream and simply deleting problematic yum repos
    instead of resorting to tricky dnf commands makes the TMT logic a lot
    easier to maintain.
    
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

--- plans/podman_e2e_test.sh
+++ plans/podman_e2e_test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-set -exo pipefail
+# Do not set -e as we want to work on all results of `rpm -q dnf[5]`.
+set -xo pipefail
 
 cat /etc/redhat-release
 rpm -q container-selinux golang podman
@@ -8,8 +9,21 @@ rpm -q container-selinux golang podman
 # /tmp is often unsufficient
 export TMPDIR=/var/tmp
 
+# dnf5 contains breaking changes
+# Either of `dnf` OR `dnf5` will be installed, never both.
+# To fetch srpm, dnf uses `--source`, dnf5 uses `--srpm`.
+rpm -q dnf5
+if [[ $? -eq 0 ]]; then
+    SRPM_OPTS="--srpm"
+else
+    SRPM_OPTS="--source"
+fi
+
 # Fetch and extract latest podman source from podman-next copr
-dnf --disablerepo=* --enablerepo=copr:copr.fedorainfracloud.org:rhcontainerbot:podman-next download --source podman
+# NOTE: The TMT preparation set podman-next copr to the highest priority so we
+# shouldn't need to manipulate any dnf repos here but just fetch from what's
+# already set.
+dnf download $SRPM_OPTS podman
 rpm2cpio podman*.src.rpm | cpio -di
 tar zxf podman-*-dev.tar.gz
 
