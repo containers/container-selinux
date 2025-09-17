a64255c8dc2d9dfe9fd77377192979001cb04a4c
Lokesh Mandvekar <lsm5@redhat.com>
2025-09-17 08:48:05 -0400

    TMT: run passt system tests from podman
    Signed-off-by: Lokesh Mandvekar <lsm5@redhat.com>

---
+++ test/podman-rootless-tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+cat /etc/redhat-release
+
+# Print versions of distro and installed packages
+rpm -q bats container-selinux passt passt-selinux podman podman-tests policycoreutils selinux-policy
+
+loginctl enable-linger "$ROOTLESS_USER"
+
+# Run podman system tests
+su - "$ROOTLESS_USER" -c "bats /usr/share/podman/test/system/410-selinux.bats"
+su - "$ROOTLESS_USER" -c "bats /usr/share/podman/test/system/500-networking.bats"
+su - "$ROOTLESS_USER" -c bats "/usr/share/podman/test/system/505-networking-pasta.bats"
